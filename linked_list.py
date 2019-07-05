c7e799801480f3c16415a043bba9405dab3cbe49
Mena Adams <menaadams@gmail.com>
2019-07-05 09:48:10 -0700

    added readme

--- linked_list.py
+++ linked_list.py
@@ -1,1 +1,10 @@
 """Linked List Implementation."""
+
+
+class Node(object):
+    """Node Class."""
+
+    def __init__(self, data=None):
+        """Initialize Node."""
+        self.data = data
+        self.next = None
--- tests.py
+++ tests.py
@@ -3,3 +3,16 @@
 import linked_list
 
 import unittest
+
+
+class TestNode(unittest.TestCase):
+    """Test node class."""
+
+    def test_node(self):
+        """Test Node creation."""
+        node = linked_list.Node("A")
+        self.assertEqual(node.data, "A")
+
+
+if __name__ == "__main__":
+    unittest.main()
